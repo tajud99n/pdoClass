ac6e0bafb7f12f84560ac7d83f8d86ef54f6add1
tajud99n <giwa208@gmail.com>
2017-11-17 14:48:36 +0100

    login page

--- www/login.php
+++ www/login.php
@@ -1,21 +1,53 @@
 <?php
 	$page_title = "Login"; 
-    include ('includes/header.php')    
+	include ('includes/header.php');
+	include 'includes/db.php';
+	include 'funct.php';
+
+	
+	$error = [];
+	if (array_key_exists('login',$_POST)){
+		if (empty($_POST['email'])) {
+			$error['email'] = "Please enter email address";
+		}
+		if (empty($_POST['password'])) {
+			$error['password'] = "Please enter password";
+		}
+		if (empty($error)) {
+			$clean =   array_map('trim',$_POST);
+			$hash = password_hash($clean['password'], PASSWORD_BCRYPT);
+
+			$stmt = $conn->prepare("SELECT * FROM admin WHERE :e=email");
+			$stmt->bindParam(":e", $_POST['email']);
+
+			$stmt->execute();
+
+			if($stmt->rowCount() == 1){
+
+				while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+				if ($result['hash'] == $hash ) {
+					//header("location:");
+				}
+			}
+		}
+	}
 ?>
 <div class="wrapper">
 		<h1 id="register-label">Admin Login</h1>
 		<hr>
 		<form id="register"  action ="login.php" method ="POST">
 			<div>
+				<?php $data = displayErrors($error, 'email'); ?>								
 				<label>email:</label>
 				<input type="text" name="email" placeholder="email">
 			</div>
 			<div>
+				<?php $data = displayErrors($error, 'password'); ?>								
 				<label>password:</label>
 				<input type="password" name="password" placeholder="password">
 			</div>
 
-			<input type="submit" name="register" value="login">
+			<input type="submit" name="login" value="login">
 		</form>
 
 		<h4 class="jumpto">Don't have an account? <a href="register.php">register</a></h4>
