42db416f5d24b49daffe9a2ba220798e031ba728
tajud99n <giwa208@gmail.com>
2017-11-22 18:23:56 +0100

    modified delete category

--- www/delete_category.php
+++ www/delete_category.php
@@ -4,40 +4,12 @@
     include 'includes/db.php';       
     include 'funct.php';    
     include 'includes/dashboard_header.php';
-    checkLogin();
+    
 
     if ($_GET['cat_id']) {
         $cat_id = $_GET['cat_id'];
     }
 
-    $item = getCategoryById($conn,$cat_id);
+    deleteCategory($conn,$cat_id);
 
-    $error = [];
-    if (array_key_exists('delete',$_POST)) {
-        if (empty($_POST['cat_name'])) {
-            $error['cat_name'] = "Please Enter Category Name";
-        }
-        if (empty($error)) {
-           $clean = array_map('trim',$_POST);
-           $clean['id'] = $cat_id;
-
-            deleteCategory($conn,$clean);
-
-        }
-    }
-?>
-<div class="wrapper">
-		<div id="stream">
-			<form id="register"  action ="" method ="POST">
-			    <div>
-                    <?php $data = displayErrors($error, 'cat_name'); ?>								
-				    <label>Edit Category:</label>
-				    <input type="text" name="cat_name" placeholder="category name" value="<?php echo $item[1]; ?>">
-                </div>
-                <input type="submit" name="delete" value="Delete">
-            </form>
-		</div>
-</div>
-<?php
-    include 'includes/footer.php'
 ?>
